4c2c49a0be84938ad7bdc487695a983388bad79a
@tblitzcr <tblitzcr@student.21-school.ru>
2023-10-25 21:32:10 +0700

    init

---
+++ SmartCalc/src/main/java/com/calculator/SmartCalc/SmartCalcApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.SmartCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCalcApplication.class, args);
+	}
+
+}
