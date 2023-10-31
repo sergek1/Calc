dfc95c801741c8712b5cb6b37eb380fa3ae8c4d3
sergek1 <serg.seden@yandex.ru>
2023-10-31 18:09:47 +0700

    react in process

---
+++ SmartCalc/src/main/view/src/App.js
@@ -0,0 +1,13 @@
+import { VideoList } from "./components/VideoList";
+
+function App() {
+  return (
+    <>
+    
+    <h1>Hello world</h1>
+    <VideoList/>
+    
+    </>);
+}
+
+export default App;
---
+++ SmartCalc/src/main/view/src/components/VideoList.jsx
@@ -0,0 +1,11 @@
+export function VideoList(){
+    return (
+    <>
+    <h1>VideoList</h1>
+    <div>
+        <p></p>
+        <p></p>
+        <button></button>
+    </div>
+    </>)
+}
