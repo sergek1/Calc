8f277c1fe5f547e767ea96d7927819295a67ea26
tblitzcr <tblitzcr@student.21-school.ru>
2023-11-21 18:53:51 +0700

    electron ready

--- SmartCalc/src/main/view/src/App.js
+++ SmartCalc/src/main/view/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route,Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route,Routes } from 'react-router-dom';
 import Graph from './components/graph';
 import Parent from './components/parent'
 
 function App() {
   
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div>
         <Routes>
           <Route path="/" element={<Parent/>} />
           <Route path="/graphLink" element={<Graph/>} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
